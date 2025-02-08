fa6df2510a41f69c5aa94f520e57fffe76d19459
Althowheed Ahmed <thowheedahmed22@gmail.com>
2025-02-08 18:28:40 +0530

    calnedar changes - commit

--- src/Navigate/Navigate.tsx
+++ src/Navigate/Navigate.tsx
@@ -3,6 +3,7 @@ import {BrowserRouter, Route, Routes, Navigate} from "react-router-dom"
 import LoginPage from "../Components/LoginPage.tsx";
 import DashboardScreen from "../Components/DashBorad.tsx";
 import ChatBox from "../Components/ChatBox.tsx";
+import CalendarComponent from "../Components/Calendar.tsx";
 
 function Navigates(){
     return(
@@ -12,6 +13,7 @@ function Navigates(){
                 <Route path="/login" element={<LoginPage/>}/>
                 <Route path="/dashboard" element={<DashboardScreen/>}/>
                 <Route path="/chatbot" element={<ChatBox/>}/>
+                <Route path="/calendar" element={<CalendarComponent/>}/>
             </Routes>
         </BrowserRouter>
     )
